12f8c5b63b85760da6e5fef4f6d918f1cdf12cc3
Danielrvrt <39166172+Danielrvrt@users.noreply.github.com>
2022-09-29 19:52:20 -0500

    Point 2 done

--- PruebaSourcecol/Program.cs
+++ PruebaSourcecol/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PruebaSourcecol;
+
+class program
+{
+
+    static void Main(string[] args)
+    {
+        Fibonacci fib = new Fibonacci();
+        fib.caller();
+
+    }
+}
