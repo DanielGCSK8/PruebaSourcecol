64096a9e19c63e45ea0b8c3ac62bb6304a74966b
Danielrvrt <39166172+Danielrvrt@users.noreply.github.com>
2022-10-01 15:00:53 -0500

    Test Done

---
+++ PruebaSourcecol/Fibonacci.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaSourcecol
+{
+    internal class Fibonacci
+    {
+        public bool fibonacci(int num)
+        {
+            double n1 = 5 * Math.Pow(num, 2) + 4;
+            double n2 = 5 * Math.Pow(num, 2) - 4;
+
+            int square_n1 = (int)Math.Sqrt(n1);
+            int square_n2 = (int)Math.Sqrt(n2);
+
+            if ((square_n1 * square_n1 == n1) || (square_n2 * square_n2 == n2))
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+            }
+
+        }
+
+        public void caller()
+        {
+            Console.WriteLine("Ingresa un número");
+            int num = int.Parse(Console.ReadLine());
+
+            if (fibonacci(num) == true)
+            {
+                Console.WriteLine("El número pertenece a la sucesión fibonacci");
+            }
+            else
+            {
+                Console.WriteLine("El número no pertenece a la sucesión fibonacci");
+            }
+
+        }
+    }
+}
--- PruebaSourcecol/Program.cs
+++ PruebaSourcecol/Program.cs
@@ -1,12 +1,42 @@
-﻿using PruebaSourcecol;
+﻿
+using Newtonsoft.Json;
+using PruebaSourcecol;
+using System.Net.Http.Headers;
 
 class program
 {
+    private static readonly HttpClient client = new HttpClient();
 
     static void Main(string[] args)
     {
+        Console.WriteLine("-----1-------   ");
         TextString txt = new TextString();
         txt.caller();
+        Console.WriteLine("-----2-------   ");
+        Fibonacci fib = new Fibonacci();
+        fib.caller();
+        Console.WriteLine("-----3-------   ");
+        ArrayOfNumbers a = new ArrayOfNumbers();
+        a.call();
+        Console.WriteLine("-----4-------   ");
+        Car c = new Car();
+        c.caller();
+        Console.WriteLine("-----Api Con los Nombres-------   ");
+        ProcessApi();
+
+    }
+
+    private static void ProcessApi()
+    {
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var streamTask = client.GetAsync("https://rickandmortyapi.com/api/character").Result;
+        var result = streamTask.Content.ReadAsStringAsync().Result;
+        var res = JsonConvert.DeserializeObject<RickMorty>(result);
+        foreach (var rm in res.Results)
+            Console.WriteLine(rm.Name);
 
     }
 }
