154c4a73ec61ae5a87f71f180c7e1e098aa1a577
Danielrvrt <39166172+Danielrvrt@users.noreply.github.com>
2022-10-01 11:01:04 -0500

    Api consumed

--- PruebaSourcecol/Program.cs
+++ PruebaSourcecol/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using PruebaSourcecol;
+using System.Net.Http.Headers;
+
+class program
+{
+
+    private static readonly HttpClient client = new HttpClient();
+
+    static void Main(string[] args)
+    {
+        ProcessApi();
+    }
+    private static void ProcessApi()
+    {
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var streamTask = client.GetAsync("https://rickandmortyapi.com/api/character").Result;
+        var result = streamTask.Content.ReadAsStringAsync().Result;
+        var res = JsonConvert.DeserializeObject<RickMorty>(result);
+        foreach (var rm in res.Results)
+            Console.WriteLine(rm.Name);
+
+    }
+}
---
+++ PruebaSourcecol/RickMorty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PruebaSourcecol
+{
+    class RickMorty
+    {
+        [JsonPropertyName("results")]
+        public List<Result> Results { get; set; }
+
+    }
+
+    class Result
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
